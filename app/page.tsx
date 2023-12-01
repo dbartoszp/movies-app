acc7b238f0f6cfa24790204ba477df1a6f8bcc7b
dbartoszp <dbartosz.pomierny@gmail.com>
2023-12-01 11:55:55 +0100

    feat/footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,20 @@
-"use client";
-import "./globals.css";
-import { HomepageBanner } from "@/modules/HomepageBanner/HomepageBanner";
-import { Link } from "@/modules/ui/Button/Link";
-import { Text } from "@/modules/Text/Text";
+'use client';
+import './globals.css';
+import { HomepageBanner } from '@/modules/HomepageBanner/HomepageBanner';
+import { Link } from '@/modules/ui/Button/Link';
+import { Text } from '@/modules/Text/Text';
 
 export default function Home() {
-  return (
-    <>
-      <div>
-        <HomepageBanner />
-        <div className="flex justify-center mt-12">
-          <Link variant="green" size="lg" href="/account">
-            <Text>Create an account now!</Text>
-          </Link>
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div>
+				<HomepageBanner />
+				<div className="flex justify-center mt-12 md:mt-6">
+					<Link variant="green" size="lg" href="/account">
+						<Text>Create an account now!</Text>
+					</Link>
+				</div>
+			</div>
+		</>
+	);
 }
--- modules/Footer/Footer.tsx
+++ modules/Footer/Footer.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React from 'react';
+import { Text } from '../Text/Text';
+import { Link } from '../ui/Button/Link';
+import { FaLinkedin } from 'react-icons/fa6';
+import { FaGithub } from 'react-icons/fa';
 
-const Footer = () => {
-  return <div>Footer</div>;
+export const Footer = () => {
+	return (
+		<footer className="absolute h-36 bottom-0 bg-dark-blue w-full">
+			<div className="mt-6 text-center flex flex-col space-y-10">
+				<div>
+					<Text>All about your favourite movies.</Text>
+				</div>
+				<div className="flex flex-col space-y-3 items-center bg-dark-blue">
+					<div className="border-b mb-4">
+						<Text>Contact us via:</Text>
+					</div>
+					<Text>Phone - 123 456 789</Text>
+					<Text>Email - bartpom034@student.polsl.pl</Text>
+					<div className="flex flex-row space-x-6">
+						<Link href="https://www.linkedin.com/in/bartosz-pomierny-b751aa274/">
+							<FaLinkedin size={30} />
+						</Link>
+						<Link href="https://github.com/dbartoszp">
+							<FaGithub size={30} />
+						</Link>
+					</div>
+				</div>
+			</div>
+		</footer>
+	);
 };
-
-export default Footer;
