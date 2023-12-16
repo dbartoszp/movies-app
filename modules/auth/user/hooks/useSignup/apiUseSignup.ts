c72264af449f93df6125ed7e8ba85756726b0b5a
dbartoszp <dbartosz.pomierny@gmail.com>
2023-12-16 13:49:45 +0100

    fix/login

--- modules/auth/user/hooks/useSignup/apiUseSignup.ts
+++ modules/auth/user/hooks/useSignup/apiUseSignup.ts
@@ -1,4 +1,4 @@
-import { createClientComponentClient } from '@supabase/ssr';
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 
 const supabase = createClientComponentClient();
 
--- services/supabase.mjs
+++ services/supabase.mjs
@@ -1,6 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
-const supabaseUrl = 'https://ctmrfwfuspkzlnxegohw.supabase.co';
-const supabaseKey =
-	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN0bXJmd2Z1c3BremxueGVnb2h3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDE0MzA5NzcsImV4cCI6MjAxNzAwNjk3N30.NFUXyaznDatBYIXNS59pquOk9SrMDXwYVBP27LqPIfg';
-const supabase = createClient(supabaseUrl, supabaseKey);
+// const supabaseUrl = 'https://ctmrfwfuspkzlnxegohw.supabase.co';
+// const supabaseKey =
+// 	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN0bXJmd2Z1c3BremxueGVnb2h3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDE0MzA5NzcsImV4cCI6MjAxNzAwNjk3N30.NFUXyaznDatBYIXNS59pquOk9SrMDXwYVBP27LqPIfg';
+const supabase = createClient(
+	process.env.NEXT_PUBLIC_SUPABASE_URL,
+	process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+);
 export default supabase;
