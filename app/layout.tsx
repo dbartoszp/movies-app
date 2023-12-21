72aee470129285b44079fecc22f96f3878dccd55
dbartoszp <dbartosz.pomierny@wp.pl>
2023-12-21 21:08:51 +0100

    feat/MovieImageComponent

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,52 +1,59 @@
 import './globals.css';
+import 'react-loading-skeleton/dist/skeleton.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { Header } from '@/modules/Header/Header';
 import { Footer } from '@/modules/Footer/Footer';
 import { ReactQueryProvider } from './ReactQueryProvider';
 import { Toaster } from 'react-hot-toast';
+import { SkeletonTheme } from 'react-loading-skeleton';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-	title: 'Movies-App',
-	description: 'Web application for browsing information movies and actors',
+  title: 'Movies-App',
+  description: 'Web application for browsing information movies and actors',
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-	return (
-		<html lang="en">
-			<body className={inter.className}>
-				<ReactQueryProvider>
-					<Header />
-					{children}
-					<Footer />
-					<Toaster
-						position="top-center"
-						gutter={12}
-						containerStyle={{ margin: '8px' }}
-						toastOptions={{
-							success: {
-								duration: 2000,
-							},
-							error: {
-								duration: 5000,
-							},
-							style: {
-								fontSize: '16px',
-								maxWidth: '500px',
-								padding: '16px 24px',
-								backgroundColor: '#dedede',
-								color: 'var(--dark-blue)',
-							},
-						}}
-					/>
-				</ReactQueryProvider>
-			</body>
-		</html>
-	);
+  return (
+    <html lang='en'>
+      <body className={inter.className}>
+        <ReactQueryProvider>
+          <SkeletonTheme
+            baseColor='#14181c'
+            highlightColor='rgb(118, 145, 177)'
+          >
+            <Header />
+            {children}
+            <Footer />
+            <Toaster
+              position='top-center'
+              gutter={12}
+              containerStyle={{ margin: '8px' }}
+              toastOptions={{
+                success: {
+                  duration: 2000,
+                },
+                error: {
+                  duration: 5000,
+                },
+                style: {
+                  fontSize: '16px',
+                  maxWidth: '500px',
+                  padding: '16px 24px',
+                  backgroundColor: '#dedede',
+                  color: 'var(--dark-blue)',
+                },
+              }}
+            />
+          </SkeletonTheme>
+        </ReactQueryProvider>
+      </body>
+    </html>
+  );
 }
--- app/movie/[movieId]/page.tsx
+++ app/movie/[movieId]/page.tsx
@@ -1,14 +1,14 @@
 'use client';
 
 import Image from 'next/image';
+import { useGetMovieById } from '@/modules/movies/useGetMovieById/useGetMovieById';
 import { MovieDescription } from '@/modules/moviePage/MovieDescription/MovieDescription';
 import { MovieMainInfo } from '@/modules/moviePage/MovieMainInfo/MovieMainInfo';
 import { MovieRatings } from '@/modules/moviePage/MovieRatings/MovieRatings';
 import { FullCastContainer } from '@/modules/moviePage/FullCastContainer/FullCastContainer';
 import { MovieAwardsContainer } from '@/modules/moviePage/MovieAwards/MovieAwardsContainer';
-import { Button } from '@/modules/ui/Button/Button';
-import { useGetMovieById } from '@/modules/movies/useGetMovieById/useGetMovieById';
 import { Text } from '@/modules/Text/Text';
+import { MovieImage } from '@/modules/moviePage/MovieImage/MovieImage';
 
 export default function MoviePage({ params }: { params: { movieId: string } }) {
   const movie = useGetMovieById(params.movieId);
@@ -25,15 +25,7 @@ export default function MoviePage({ params }: { params: { movieId: string } }) {
 
   return (
     <div className='flex min-h-screen flex-col items-center justify-center md:mt-36 md:flex-row'>
-      <div className='mb-8 w-full md:mb-0 md:w-1/5'>
-        <Image
-          src={movieData.image}
-          width={600}
-          height={1000}
-          alt={`${movieData.title} poster`}
-          layout='responsive'
-        />
-      </div>
+      <MovieImage image={movieData.image} title={movieData.title} />
 
       <div className='flex w-full flex-col space-y-4 md:w-1/2 md:items-center'>
         <MovieMainInfo
