31e7ebb81efecdb474e427ee9ace9b4bd82dce9d
dbartoszp <dbartosz.pomierny@wp.pl>
2023-12-28 13:07:00 +0100

    fix/searchResultsPage

--- modules/Search/SearchResultsPage/SearchResultsPage.tsx
+++ modules/Search/SearchResultsPage/SearchResultsPage.tsx
@@ -7,12 +7,13 @@ import { ErrorMessage } from '@/modules/ui/ErrorMessage/ErrorMessage';
 import { useSearchParams } from 'next/navigation';
 import { SearchResultsPageSkeleton } from './SearchResultsPageSkeleton/SearchResultsPageSkeleton';
 import { SearchResultsPageEmptyQuery } from './SearchResultsPageEmptyQuery/SearchResultsPageEmptyQuery';
+import { useState } from 'react';
 
 export const SearchResultsPage = () => {
   const searchParams = useSearchParams();
 
   const titleQuery = searchParams.get('query') || '';
-  const pageQuery = searchParams.get('page') || 1;
+  const pageQuery = searchParams.get('page') || 1; //TODO in case of pagination
 
   const movies = useGetMoviesByTitle(titleQuery);
 
@@ -21,7 +22,12 @@ export const SearchResultsPage = () => {
   if (!movies.isSuccess) return <ErrorMessage />;
 
   return (
-    <div className='bg-dark-blue md:mx-24 md:my-12 md:p-6'>
+    <div className='bg-dark-blue pt-4 md:mx-24 md:my-12 md:p-6'>
+      <div className='ml-4'>
+        <Text variant='title'>
+          Search results for &apos;<strong>{titleQuery}</strong>&apos;:
+        </Text>
+      </div>
       <MoviesList movies={movies.data.results} />
     </div>
   );
