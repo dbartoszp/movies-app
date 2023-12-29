cf9a7426f0cae7a57f6cdf075d6d3624d9d06c0e
dbartoszp <dbartosz.pomierny@wp.pl>
2023-12-29 16:51:23 +0100

    feat/reviewCreator

--- app/reviewCreator/[movieId]/page.tsx
+++ app/reviewCreator/[movieId]/page.tsx
@@ -9,6 +9,7 @@ import { Text } from '@/modules/ui/Text/Text';
 import { useState } from 'react';
 
 const MAX_RATING = 10;
+const CHARACTER_LIMIT = 100;
 
 export default function ReviewCreatorPage({
   params,
@@ -45,33 +46,55 @@ export default function ReviewCreatorPage({
   if (isSubmitted) return <Text>Review submitted!</Text>;
 
   return (
-    <main className='bg-dark-blue p-6'>
+    <main className='flex flex-col space-y-6 p-6'>
       <Text variant='title'>
         Create a review for <strong>{movie.data.fullTitle}</strong>
       </Text>
-      <Text>Select your rating from 1 to 10:</Text>
-      {Array.from({ length: MAX_RATING }, (_, index) => (
-        <RatingButton
-          isHighlighted={rating === index + 1}
-          key={index}
-          onRating={() => handleRatingChange(index + 1)}
-          rating={index + 1}
-        />
-      ))}
+      <div className='flex flex-col items-center justify-center bg-dark-blue p-4'>
+        <Text>Select your rating from 1 to 10:</Text>
+        <div className='mt-4'>
+          {Array.from({ length: MAX_RATING }, (_, index) => (
+            <RatingButton
+              isHighlighted={rating === index + 1}
+              key={index}
+              onRating={() => handleRatingChange(index + 1)}
+              rating={index + 1}
+            />
+          ))}
+        </div>
+      </div>
       <Text>Write your review:</Text>
-      <textarea
-        value={writtenReview}
-        onChange={handleWrittenReviewChange}
-        className='w-full rounded-lg bg-light-blue-lighter p-2 text-dark-blue'
-      />
-      <Text variant='subtitleXL'>Summary:</Text>
-      <Text>
-        {movie.data.fullTitle} - {rating}/10
+      <div>
+        <textarea
+          maxLength={CHARACTER_LIMIT}
+          value={writtenReview}
+          onChange={handleWrittenReviewChange}
+          className='h-36 w-full resize-none bg-dark-blue p-4'
+        />
+      </div>
+      <Text variant='subtitle'>
+        {CHARACTER_LIMIT - writtenReview.length} characters left
       </Text>
-      <Text variant='subtitle'>{writtenReview}</Text>
-      <Button variant='green' size='lg' onClick={handleSubmitReview}>
-        Submit your review!
-      </Button>
+      <Text variant='subtitleXL'>Summary:</Text>
+      <div>
+        <Text>
+          {movie.data.fullTitle} - <strong>{rating}/10</strong>
+        </Text>
+        {writtenReview !== '' && (
+          <div className='overflow-none break-words italic'>
+            <Text variant='subtitle'>&quot;{writtenReview}&quot;</Text>
+          </div>
+        )}
+      </div>
+      {rating !== 0 && writtenReview.trim() !== '' ? (
+        <Button variant='green' size='lg' onClick={handleSubmitReview}>
+          Submit your review!
+        </Button>
+      ) : (
+        <Text variant='danger'>
+          Please rate the movie and write the review first!
+        </Text>
+      )}
     </main>
   );
 }
